41d321f3d8e050f112957c37dadb1bc59e393a62
DavidA1996 <98842117+DavidA1996@users.noreply.github.com>
2022-05-11 16:18:30 +0200

    Initial Commit

---
+++ src/GameUI.java
@@ -0,0 +1,41 @@
+import processing.core.PApplet;
+
+public class GameUI extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("GameUI");
+
+    }
+
+    int xPosRect = 50;
+    int yPosRect = 50;
+    int xPosEllipse = 55;
+    int yPosEllipse = 50;
+
+    NimGame n1 = Nim.of(2, 3, 4);
+
+
+    public void settings() {
+        size(1280, 720);
+    }
+
+    public void draw() {
+
+
+        background(0);
+
+
+        //text(n1.getRows(), 500, 500);
+        System.out.println(n1.getRows());
+
+        for (int i = 1; i < n1.getRows().length; i++) {
+            fill(251, 191, 119);
+            rect(xPosRect, yPosRect, 10, 100);
+            fill(255);
+            ellipse(xPosEllipse, yPosEllipse, 10, 20);
+
+            xPosRect += 100;
+            xPosEllipse += 100;
+        }
+    }
+
+}
